1a8bea0331a720ea11f96c48024503c14a1ecd93
Teri Jensen <teriannjensen@gmail.com>
2021-10-09 15:54:42 -0600

    JS questions added, still needs to continue

---
+++ script.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question:'Which is the oldest of the 3 below variable options',
+        options: ['CONST', 'VAR', 'LET'],
+        answer: 'VAR',
+    },
+    {
+        question: 'Which of the following is the correct way to type multiple words in JavaScript?',
+        options: ['two-words','Two-Words', 'twoWords', 'TwoWords'],
+        answer: 'twoWords',
+    },
+    {
+        question: 'A variable name cannot start with a ______',
+        options: ['number', 'capital', 'letter'],
+        answer: 'number',
+    },
+    {
+        question: 'Reference value types DO NOT include which of the following',
+        options: ['function', 'numbers', 'array', 'object'],
+        answer: 'numbers',
+    },
+    {
+        question: 'the <script> element linking the JavaScript in your html should be located',
+        options: ['near the top','near the middle', 'near the bottom'],
+        answer: 'near the bottom',
+    }
+];
