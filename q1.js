0f91f7d47937bc7899ae4dd4f856138c87bac494
Teri Jensen <teriannjensen@gmail.com>
2021-10-12 16:21:41 -0600

    timer work

---
+++ q1.js
@@ -0,0 +1,8 @@
+document.getElementById("q0-a0").innerHTML = questions[0].options[0]
+document.getElementById("q0-a1").innerHTML = questions[0].options[1]
+document.getElementById("q0-a2").innerHTML = questions[0].options[2]
+document.getElementById("q0-a3").innerHTML = questions[0].options[3]
+
+localStorage.getItem("time")
+
+console.log(time)
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const questions = [
     {
         question:'Which is the oldest of the 3 below variable options',
-        options: ['CONST', 'VAR', 'LET'],
+        options: ['CONST', 'VAR', 'LET', 'N/A'],
         answer: 'VAR',
     },
     {
@@ -26,3 +26,24 @@ const questions = [
         answer: 'near the bottom',
     }
 ];
+
+// Start Quiz
+
+function wrongAnswer(){
+    time = time-10;
+}
+
+// Timer
+var startTimer = document.getElementById("timer");
+var time = 90
+
+function startQuizTimer(){
+    //location.href = "file:///C:/Users/teria/OneDrive/Desktop/Challenges/Challenge-Four/JavaScript-Quizzerie/q1.html"
+    setInterval(function(){ 
+        startTimer.innerHTML = time
+        time--; 
+        console.log(time)
+        localStorage.setItem("time", time);
+    }, 1000);
+    
+}
